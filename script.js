68031e661ccfd5dec450ec8f2bdfd4126ed46c44
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-07-09 08:53:34 -0700

    Complete game features for playing game
    Implemented playGame func to playRound 5 times
    Fixed playRound output messages for winners

--- script.js
+++ script.js
@@ -8,7 +8,6 @@
          random number equals 2: RETURN "paper"
          random number equals 3: RETURN "scissors"
 */
-
 function getComputerChoice() {
    let randomNumber = Math.floor(Math.random() * 3) + 1;
 
@@ -26,4 +25,120 @@ function getComputerChoice() {
          return;
    }
 }
+
+/* 
+   GET user input with input being 1-3 (inclusive)
+   RETURN rock, paper, scissors based off input
+      1 - rock; 2 - paper; 3 - scissors
    
+      PROMPT user to enter
+         1 for rock, 2 for paper, or 3 for scissors
+      PARSE input to an integer
+      RETURN rock, paper, or scissors based off choice
+*/
+function getHumanChoice() {
+   let userInput = prompt(`Decide your choice based off the following:
+      1 - rock
+      2 - paper
+      3 - scissors`);
+   
+   switch (+userInput) {
+      case 1:
+         return "rock";
+         break;
+      case 2: 
+         return "paper";
+         break;
+      case 3:
+         return "scissors";
+         break;
+      default:
+         return;
+   }
+}
+
+// Scores
+let humanScore = 0;
+let computerScore = 0;
+
+// IF player chooses rock THEN
+//    IF computer chose rock, paper, or scissors THEN
+//       decide winner/loser based off choice and log winner to console in message 
+//          “You lose! PAPER beats ROCK" or “You win! ROCK beats SCISSORS" 
+//       BUMP winners score  
+// IF player chooses paper THEN
+//    IF computer chose rock, paper, or scissors THEN
+//       decide winner/loser based off choice and log winner to console in message 
+//          “You lose! SCISSORS beats PAPER" or “You win! PAPER beats ROCK" 
+//       BUMP winners score   
+// ELSE player has chosen scissors
+//    IF computer chose rock, paper, or scissors THEN
+//       decide winner/loser based off choice and log winner to console in message 
+//          “You lose! ROCK beats SCISSORS" or “You win! SCISSORS beats PAPER" 
+//       BUMP winners score
+// ENDIF
+function playRound(humanChoice, computerChoice) {
+   humanChoice = humanChoice.toUpperCase();
+   computerChoice = computerChoice.toUpperCase();
+   
+   switch (humanChoice) {
+      case "ROCK":
+         switch (computerChoice) {
+            case "ROCK":
+               console.log("TIE!");
+               break;
+            case "PAPER":
+               console.log(`You lose ${computerChoice} beats ${humanChoice}`);
+               computerScore++;
+               break;
+            default:
+               console.log(`You win ${humanChoice} beats ${computerChoice}`);
+               humanScore++;
+               break;
+         }
+         break;
+      case "PAPER":
+         switch (computerChoice) {
+            case "ROCK":
+               console.log(`You win ${humanChoice} beats ${computerChoice}`);
+               humanScore++;
+               break;
+            case "PAPER":
+               console.log("TIE!");
+               break;
+            default:
+               console.log(`You lose ${computerChoice} beats ${humanChoice}`);
+               computerScore++;
+               break;
+         }
+         break;
+      default:
+         switch (computerChoice) {
+            case "ROCK":
+               console.log(`You lose ${computerChoice} beats ${humanChoice}`);
+               computerScore++;
+               break;
+            case "PAPER":
+               console.log(`You win ${humanChoice} beats ${computerChoice}`);
+               humanScore++;
+               break;
+            default:
+               console.log("TIE!");
+               break;
+         }
+         break;
+   }
+}
+
+// implement playGame function
+function playGame() {
+   for (let i = 0; i < 5; i++) {
+      playRound(getHumanChoice(), getComputerChoice());
+   }
+
+   console.log("FINAL SCORE");
+   console.log("Human: " + humanScore);
+   console.log("Computer: " + computerScore);
+}
+
+playGame();
